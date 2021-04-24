bd22a649dde5872ca934ce1945fec2427d480f88
wangqj <qpalzmqaz123@gmail.com>
2021-04-24 10:32:40 +0800

    fix atomic_shim

--- src/atomic_shim.rs
+++ src/atomic_shim.rs
@@ -1,4 +1,4 @@
-///! Inline of https://github.com/bltavares/atomic-shim
+///! Inline of `https://github.com/bltavares/atomic-shim`
 
 #[cfg(not(any(
     target_arch = "mips",
@@ -8,60 +8,35 @@
 pub use std::sync::atomic::{AtomicI64, AtomicU64};
 #[cfg(any(target_arch = "mips", target_arch = "powerpc", feature = "mutex"))]
 mod shim {
-    use crossbeam_utils::sync::ShardedLock;
+    use parking_lot::{const_rwlock, RwLock};
     use std::sync::atomic::Ordering;
 
     #[derive(Debug, Default)]
     pub struct AtomicU64 {
-        value: ShardedLock<u64>,
+        value: RwLock<u64>,
     }
 
     impl AtomicU64 {
-        pub fn new(v: u64) -> Self {
-            Self { value: ShardedLock::new(v) }
-        }
-
-        #[allow(dead_code)]
-        pub fn get_mut(&mut self) -> &mut u64 {
-            self.value.get_mut().unwrap()
-        }
-
-        #[allow(dead_code)]
-        pub fn into_inner(self) -> u64 {
-            self.value.into_inner().unwrap()
+        pub const fn new(v: u64) -> Self {
+            Self { value: const_rwlock(v) }
         }
 
         #[allow(dead_code)]
         pub fn load(&self, _: Ordering) -> u64 {
-            *self.value.read().unwrap()
+            *self.value.read()
         }
 
         #[allow(dead_code)]
         pub fn store(&self, value: u64, _: Ordering) {
-            let mut lock = self.value.write().unwrap();
+            let mut lock = self.value.write();
             *lock = value;
         }
 
         #[allow(dead_code)]
         pub fn swap(&self, value: u64, _: Ordering) -> u64 {
-            let mut lock = self.value.write().unwrap();
+            let mut lock = self.value.write();
             let prev = *lock;
             *lock = value;
-            prev
-        }
-
-        #[allow(dead_code)]
-        pub fn compare_and_swap(
-            &self,
-            current: u64,
-            new: u64,
-            _: Ordering,
-        ) -> u64 {
-            let mut lock = self.value.write().unwrap();
-            let prev = *lock;
-            if prev == current {
-                *lock = new;
-            };
             prev
         }
 
@@ -73,13 +48,30 @@ mod shim {
             _: Ordering,
             _: Ordering,
         ) -> Result<u64, u64> {
-            let mut lock = self.value.write().unwrap();
+            let mut lock = self.value.write();
             let prev = *lock;
             if prev == current {
                 *lock = new;
                 Ok(current)
             } else {
                 Err(prev)
+            }
+        }
+
+        #[allow(dead_code)]
+        pub fn compare_and_swap(
+            &self,
+            current: u64,
+            new: u64,
+            _: Ordering,
+        ) -> u64 {
+            let mut lock = self.value.write();
+            let prev = *lock;
+            if prev == current {
+                *lock = new;
+                current
+            } else {
+                prev
             }
         }
 
@@ -96,7 +88,7 @@ mod shim {
 
         #[allow(dead_code)]
         pub fn fetch_add(&self, val: u64, _: Ordering) -> u64 {
-            let mut lock = self.value.write().unwrap();
+            let mut lock = self.value.write();
             let prev = *lock;
             *lock = prev.wrapping_add(val);
             prev
@@ -104,7 +96,7 @@ mod shim {
 
         #[allow(dead_code)]
         pub fn fetch_sub(&self, val: u64, _: Ordering) -> u64 {
-            let mut lock = self.value.write().unwrap();
+            let mut lock = self.value.write();
             let prev = *lock;
             *lock = prev.wrapping_sub(val);
             prev
@@ -112,7 +104,7 @@ mod shim {
 
         #[allow(dead_code)]
         pub fn fetch_and(&self, val: u64, _: Ordering) -> u64 {
-            let mut lock = self.value.write().unwrap();
+            let mut lock = self.value.write();
             let prev = *lock;
             *lock = prev & val;
             prev
@@ -120,7 +112,7 @@ mod shim {
 
         #[allow(dead_code)]
         pub fn fetch_nand(&self, val: u64, _: Ordering) -> u64 {
-            let mut lock = self.value.write().unwrap();
+            let mut lock = self.value.write();
             let prev = *lock;
             *lock = !(prev & val);
             prev
@@ -128,7 +120,7 @@ mod shim {
 
         #[allow(dead_code)]
         pub fn fetch_or(&self, val: u64, _: Ordering) -> u64 {
-            let mut lock = self.value.write().unwrap();
+            let mut lock = self.value.write();
             let prev = *lock;
             *lock = prev | val;
             prev
@@ -136,7 +128,7 @@ mod shim {
 
         #[allow(dead_code)]
         pub fn fetch_xor(&self, val: u64, _: Ordering) -> u64 {
-            let mut lock = self.value.write().unwrap();
+            let mut lock = self.value.write();
             let prev = *lock;
             *lock = prev ^ val;
             prev
@@ -151,55 +143,30 @@ mod shim {
 
     #[derive(Debug, Default)]
     pub struct AtomicI64 {
-        value: ShardedLock<i64>,
+        value: RwLock<i64>,
     }
 
     impl AtomicI64 {
-        pub fn new(v: i64) -> Self {
-            Self { value: ShardedLock::new(v) }
-        }
-
-        #[allow(dead_code)]
-        pub fn get_mut(&mut self) -> &mut i64 {
-            self.value.get_mut().unwrap()
-        }
-
-        #[allow(dead_code)]
-        pub fn into_inner(self) -> i64 {
-            self.value.into_inner().unwrap()
+        pub const fn new(v: i64) -> Self {
+            Self { value: const_rwlock(v) }
         }
 
         #[allow(dead_code)]
         pub fn load(&self, _: Ordering) -> i64 {
-            *self.value.read().unwrap()
+            *self.value.read()
         }
 
         #[allow(dead_code)]
         pub fn store(&self, value: i64, _: Ordering) {
-            let mut lock = self.value.write().unwrap();
+            let mut lock = self.value.write();
             *lock = value;
         }
 
         #[allow(dead_code)]
         pub fn swap(&self, value: i64, _: Ordering) -> i64 {
-            let mut lock = self.value.write().unwrap();
+            let mut lock = self.value.write();
             let prev = *lock;
             *lock = value;
-            prev
-        }
-
-        #[allow(dead_code)]
-        pub fn compare_and_swap(
-            &self,
-            current: i64,
-            new: i64,
-            _: Ordering,
-        ) -> i64 {
-            let mut lock = self.value.write().unwrap();
-            let prev = *lock;
-            if prev == current {
-                *lock = new;
-            };
             prev
         }
 
@@ -211,13 +178,30 @@ mod shim {
             _: Ordering,
             _: Ordering,
         ) -> Result<i64, i64> {
-            let mut lock = self.value.write().unwrap();
+            let mut lock = self.value.write();
             let prev = *lock;
             if prev == current {
                 *lock = new;
                 Ok(current)
             } else {
                 Err(prev)
+            }
+        }
+
+        #[allow(dead_code)]
+        pub fn compare_and_swap(
+            &self,
+            current: i64,
+            new: i64,
+            _: Ordering,
+        ) -> i64 {
+            let mut lock = self.value.write();
+            let prev = *lock;
+            if prev == current {
+                *lock = new;
+                current
+            } else {
+                prev
             }
         }
 
@@ -234,7 +218,7 @@ mod shim {
 
         #[allow(dead_code)]
         pub fn fetch_add(&self, val: i64, _: Ordering) -> i64 {
-            let mut lock = self.value.write().unwrap();
+            let mut lock = self.value.write();
             let prev = *lock;
             *lock = prev.wrapping_add(val);
             prev
@@ -242,7 +226,7 @@ mod shim {
 
         #[allow(dead_code)]
         pub fn fetch_sub(&self, val: i64, _: Ordering) -> i64 {
-            let mut lock = self.value.write().unwrap();
+            let mut lock = self.value.write();
             let prev = *lock;
             *lock = prev.wrapping_sub(val);
             prev
@@ -250,7 +234,7 @@ mod shim {
 
         #[allow(dead_code)]
         pub fn fetch_and(&self, val: i64, _: Ordering) -> i64 {
-            let mut lock = self.value.write().unwrap();
+            let mut lock = self.value.write();
             let prev = *lock;
             *lock = prev & val;
             prev
@@ -258,7 +242,7 @@ mod shim {
 
         #[allow(dead_code)]
         pub fn fetch_nand(&self, val: i64, _: Ordering) -> i64 {
-            let mut lock = self.value.write().unwrap();
+            let mut lock = self.value.write();
             let prev = *lock;
             *lock = !(prev & val);
             prev
@@ -266,7 +250,7 @@ mod shim {
 
         #[allow(dead_code)]
         pub fn fetch_or(&self, val: i64, _: Ordering) -> i64 {
-            let mut lock = self.value.write().unwrap();
+            let mut lock = self.value.write();
             let prev = *lock;
             *lock = prev | val;
             prev
@@ -274,7 +258,7 @@ mod shim {
 
         #[allow(dead_code)]
         pub fn fetch_xor(&self, val: i64, _: Ordering) -> i64 {
-            let mut lock = self.value.write().unwrap();
+            let mut lock = self.value.write();
             let prev = *lock;
             *lock = prev ^ val;
             prev
